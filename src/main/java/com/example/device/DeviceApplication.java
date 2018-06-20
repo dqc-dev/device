b7de91da301b8670735ed791e8faadae93d8bca4
soleray <pein_zero@126.com>
2018-06-20 19:02:39 +0800

    init

---
+++ src/main/java/com/example/device/DeviceApplication.java
@@ -0,0 +1,43 @@
+package com.example.device;
+
+import com.example.device.constants.MqttConstants;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+
+@SpringBootApplication
+@IntegrationComponentScan
+public class DeviceApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceApplication.class);
+    }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(MqttConstants.MQTT_HOST,MqttConstants.MQTT_CLIENT_ID ,"dev/down/+/00010010020001220161110002000968");
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setOutputChannel(new DirectChannel());
+        return adapter;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/device/constants/MqttConstants.java
@@ -0,0 +1,8 @@
+package com.example.device.constants;
+
+public interface MqttConstants {
+
+    String MQTT_HOST = "tcp://192.168.6.123:1883";
+
+    String MQTT_CLIENT_ID = "testClient";
+}
